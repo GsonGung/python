0d4d6d4385733d82dc5e1e9975a511f0dc71e526
gongjd <gongjd@centaline.com.cn>
2017-05-03 14:21:25 +0800

    简化游戏

--- datastruct/finger_game.py
+++ datastruct/finger_game.py
@@ -3,23 +3,22 @@
 
 import random
 while 1:
-    s = int(random.uniform(1, 3))
-    if s == 1:
-        ind = "石头"
-    elif s == 2:
-        ind = "剪子"
-    elif s == 3:
-        ind = "布"
-    m = raw_input('输入 石头、剪子、布,输入"end"结束游戏:')
-    blist = ['石头', "剪子", "布"]
+    s_ = int(random.uniform(1, 3))
+    s = str(s_)
+    m = raw_input('输入 1（石头）、2（剪子）、3（布）,输入"end"结束游戏:')
+    alist = ["石头","剪刀","布"]
+    blist = ["1","2","3"]
     if (m not in blist) and (m != 'end'):
         print "输入错误，请重新输入！"
     elif (m not in blist) and (m == 'end'):
         print "\n游戏退出中..."
         break
-    elif m == ind :
-        print "电脑出了： " + ind + "，平局！"
-    elif (m == '石头' and ind =='剪子') or (m == '剪子' and ind =='布') or (m == '布' and ind =='石头'):
-        print "电脑出了： " + ind +"，你赢了！"
-    elif (m == '石头' and ind =='布') or (m == '剪子' and ind =='石头') or (m == '布' and ind =='剪子'):
-        print "电脑出了： " + ind +"，你输了！"
+    elif m == s :
+        print "电脑出了： " + s+"("+alist[s_-1]+")"+  "，平局！"
+    elif (m == '1' and s =='2') or (m == '2' and s =='3') or (m == '3' and s =='1'):
+        print "电脑出了： " + s+"("+alist[s_-1]+")"+ "，你赢了！"
+    elif (m == '1' and s =='3') or (m == '2' and s =='1') or (m == '3' and s =='2'):
+        print "电脑出了： " + s+"("+alist[s_-1]+")"+ "，你输了！"
+    else :
+        print "m:"+m+"   s:"+s
+        print "未知错误！"
